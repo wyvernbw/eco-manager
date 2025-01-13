aee51d6900c76d1a95af868f95d98d2470769f5b
Your Name <new-email@example.com>
2025-01-13 11:52:05 +0200

    add types for system params

--- src/api.ts
+++ src/api.ts
@@ -6,6 +6,9 @@ const systemStatusSchema = z.object({
 	electricity: z.enum(['on', 'off', 'unknown']),
 });
 
+type SystemStatus = z.infer<typeof systemStatusSchema>;
+type SystemParams = keyof SystemStatus;
+
 const energySchema = z.object({
 	energy: z.number(),
 });
