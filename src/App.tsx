53e6194d84fc0387eb175e871a129d7608a3ad31
wyvernbw <iamwyvern511@gmail.com>
2025-01-13 12:46:00 +0200

    (wip) add usage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
+<<<<<<< HEAD
 import "./App.css";
 import Navbar from "./components/ui/Navbar/Navbar";
 import ResourceCalendar from "./components/ui/Calendar/Calendar";
+||||||| parent of 230210a ((wip) add usage component)
+import "./App.css";
+import Navbar from "./components/ui/Navbar/navbar";
+=======
+import './App.css';
+import Navbar from './components/ui/Navbar/Navbar';
+import Usage from './components/Usage';
+>>>>>>> 230210a ((wip) add usage component)
 
 function App() {
+<<<<<<< HEAD
   return (
     <>
       <Navbar></Navbar>
       <ResourceCalendar></ResourceCalendar>
     </>
   );
+||||||| parent of 230210a ((wip) add usage component)
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+=======
+	return (
+		<div className="flex flex-col h-screen w-screen">
+			<header>
+				<Navbar />
+			</header>
+			<main className="flex flex-col p-4 gap-4 mt-16">
+				<Usage type="heating" />
+				<Usage type="water" />
+				<Usage type="electricity" />
+			</main>
+		</div>
+	);
+>>>>>>> 230210a ((wip) add usage component)
 }
 
 export default App;
--- src/components/Usage.tsx
+++ src/components/Usage.tsx
@@ -1,7 +1,49 @@
+import { getSystemStatus, getUsage, SystemParams } from '@/api';
 import { useQuery } from 'react-query';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from './ui/card';
+import { Input } from './ui/input';
+import { Progress } from './ui/progress';
+import { useState } from 'react';
 
-function Usage(props: { type: string }) {
-	const usage = useQuery(props.type);
+function Usage(props: { type: SystemParams }) {
+	const status = useQuery(props.type, () => getSystemStatus());
+	const usageData = useQuery(props.type, () => getUsage());
+	const budgets = useState({
+		heating: 0,
+		water: 0,
+		electricity: 0,
+	});
+	const budget = budgets[0][props.type];
+	if (!usageData.data) return <div>Loading...</div>;
+	const usage = usageData.data[props.type] ?? 0;
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>{props.type}</CardTitle>
+				<CardDescription>Monthly {props.type} usage</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<div className="flex gap-2 items-center">
+					<span>Budget:</span>
+					<Input
+						className="w-32"
+						onChange={e =>
+							(budgets[0][props.type] = e.target.value as number)
+						}
+					/>
+					<Progress value={(usage / budget) * 100} />
+				</div>
+			</CardContent>
+			<CardFooter></CardFooter>
+		</Card>
+	);
 }
 
 export default Usage;
