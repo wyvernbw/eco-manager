91189c1a7a8645763442aba9bca8a862f759e579
Alexandru Avram <alexandruavram46@gmail.com>
2025-01-13 12:46:45 +0200

    login page created

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,18 @@ import Navbar from "./components/ui/Navbar/Navbar";
 import ResourceCalendar from "./components/ui/Calendar/Calendar";
 ||||||| parent of 230210a ((wip) add usage component)
 import "./App.css";
+<<<<<<< HEAD
 import Navbar from "./components/ui/Navbar/navbar";
 =======
 import './App.css';
 import Navbar from './components/ui/Navbar/Navbar';
 import Usage from './components/Usage';
 >>>>>>> 230210a ((wip) add usage component)
+||||||| parent of c392d1f (login page created)
+import Navbar from "./components/ui/Navbar/navbar";
+=======
+import Navbar from "./components/ui/Navbar/Navbar";
+>>>>>>> c392d1f (login page created)
 
 function App() {
 <<<<<<< HEAD
---
+++ src/Login.tsx
@@ -0,0 +1,12 @@
+import { ChartNetwork } from "lucide-react";
+
+
+const Login = () => {
+    return (
+        <div className="login-container">
+
+        </div>
+    );
+};
+
+export default Login;
