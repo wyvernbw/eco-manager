64b50124456acae23e2383f739e8a6805a10565a
wyvernbw <iamwyvern511@gmail.com>
2025-01-13 10:47:11 +0200

    add data model

---
+++ src/api.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+const systemStatusSchema = z.object({
+	heating: z.enum(['on', 'off', 'unknown']),
+	ac: z.enum(['on', 'off', 'unknown']),
+	water: z.enum(['connected', 'unknown']),
+	electricity: z.enum(['on', 'off', 'unknown']),
+});
+
+const energySchema = z.object({
+	energy: z.number(),
+});
+
+const temperatureSchema = z.object({
+	temperature: z.number(),
+});
+
+async function getEnergy() {
+	await fetch('192.167.0.40/api/energy')
+		.then(res => res.json())
+		.then(res => energySchema.parse(res));
+}
+
+async function getTemperature() {
+	await fetch('192.167.0.40/api/temperature')
+		.then(res => res.json())
+		.then(res => temperatureSchema.parse(res));
+}
+
+async function getSystemStatus() {
+	await fetch('192.167.0.40/api/system')
+		.then(res => res.json())
+		.then(res => systemStatusSchema.parse(res));
+}
