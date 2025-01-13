230210acb1e0a1eff9c5f4afa5823b355637d4cf
wyvernbw <iamwyvern511@gmail.com>
2025-01-13 12:46:00 +0200

    (wip) add usage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import "./App.css";
-import Navbar from "./components/ui/Navbar/navbar";
+import './App.css';
+import Navbar from './components/ui/Navbar/Navbar';
+import Usage from './components/Usage';
 
 function App() {
-  return (
-    <>
-      <Navbar></Navbar>
-    </>
-  );
+	return (
+		<div className="flex flex-col h-screen w-screen">
+			<header>
+				<Navbar />
+			</header>
+			<main className="flex flex-col p-4 gap-4 mt-16">
+				<Usage type="heating" />
+				<Usage type="water" />
+				<Usage type="electricity" />
+			</main>
+		</div>
+	);
 }
 
 export default App;
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
+import { get } from 'http';
 import { z } from 'zod';
 
-const systemStatusSchema = z.object({
+export const systemStatusSchema = z.object({
 	heating: z.enum(['on', 'off', 'unknown']),
 	water: z.enum(['connected', 'unknown']),
 	electricity: z.enum(['on', 'off', 'unknown']),
@@ -9,38 +10,56 @@ const systemStatusSchema = z.object({
 export type SystemStatus = z.infer<typeof systemStatusSchema>;
 export type SystemParams = keyof SystemStatus;
 
-const energySchema = z.object({
-	energy: z.number(),
+export const electricitySchema = z.object({
+	electricity: z.number(),
 });
 
-const temperatureSchema = z.object({
+export const temperatureSchema = z.object({
 	temperature: z.number(),
 });
 
-const waterSchema = z.object({
+export const waterSchema = z.object({
 	water: z.number(),
 });
 
-async function getEnergy() {
-	await fetch('192.167.0.40/api/energy')
-		.then(res => res.json())
-		.then(res => energySchema.parse(res));
+export const heatingSchema = z.object({
+	heating: z.number(),
+});
+
+export async function getUsage() {
+	return {
+		...(await getEnergy()),
+		...(await getHeating()),
+		...(await getWater()),
+	};
 }
 
-async function getTemperature() {
-	await fetch('192.167.0.40/api/temperature')
+export async function getEnergy() {
+	return await fetch('192.167.0.40/api/energy')
+		.then(res => res.json())
+		.then(res => electricitySchema.parse(res));
+}
+
+export async function getHeating() {
+	return await fetch('192.167.0.40/api/heating')
+		.then(res => res.json())
+		.then(res => heatingSchema.parse(res));
+}
+
+export async function getTemperature() {
+	return await fetch('192.167.0.40/api/temperature')
 		.then(res => res.json())
 		.then(res => temperatureSchema.parse(res));
 }
 
-async function getWater() {
-	await fetch('192.167.0.40/api/water')
+export async function getWater() {
+	return await fetch('192.167.0.40/api/water')
 		.then(res => res.json())
 		.then(res => waterSchema.parse(res));
 }
 
-async function getSystemStatus() {
-	await fetch('192.167.0.40/api/system')
+export async function getSystemStatus() {
+	return await fetch('192.167.0.40/api/system')
 		.then(res => res.json())
 		.then(res => systemStatusSchema.parse(res));
 }
