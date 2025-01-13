27f1d32fe5df449fd93318230abdfaa897f5b571
wyvernbw <iamwyvern511@gmail.com>
2025-01-13 14:04:55 +0200

    add usage

--- src/components/Usage.tsx
+++ src/components/Usage.tsx
@@ -1,4 +1,4 @@
-import { getSystemStatus, getUsage, SystemParams } from '@/api';
+import { getSystemStatus, getUsage, SystemParams, toUnits } from '@/api';
 import { useQuery } from 'react-query';
 import {
 	Card,
@@ -10,19 +10,34 @@ import {
 } from './ui/card';
 import { Input } from './ui/input';
 import { Progress } from './ui/progress';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Usage(props: { type: SystemParams }) {
-	const status = useQuery(props.type, () => getSystemStatus());
-	const usageData = useQuery(props.type, () => getUsage());
-	const budgets = useState({
+	//const status = useQuery(props.type, () => getSystemStatus());
+	const usageData = useQuery({
+		queryKey: props.type,
+		refetchInterval: 2000,
+		queryFn: async () => {
+			return await getUsage();
+		},
+	});
+	const [budgets, setBudgets] = useState({
 		heating: 0,
 		water: 0,
 		electricity: 0,
 	});
-	const budget = budgets[0][props.type];
+	const [budgetsText, setBudgetsText] = useState({
+		heating: '0',
+		water: '0',
+		electricity: '0',
+	});
+	useEffect(() => {
+		console.log('BUDGETS: ', budgets);
+	}, [budgets]);
+
 	if (!usageData.data) return <div>Loading...</div>;
 	const usage = usageData.data[props.type] ?? 0;
+	const budget = budgets[props.type];
 	return (
 		<Card>
 			<CardHeader>
@@ -30,15 +45,23 @@ function Usage(props: { type: SystemParams }) {
 				<CardDescription>Monthly {props.type} usage</CardDescription>
 			</CardHeader>
 			<CardContent>
+				<div className="my-2 font-semibold">
+					Current usage: {usage} {toUnits(props.type)}
+				</div>
 				<div className="flex gap-2 items-center">
 					<span>Budget:</span>
 					<Input
 						className="w-32"
-						onChange={e =>
-							(budgets[0][props.type] = parseFloat(
-								e.target.value
-							))
-						}
+						onChange={e => {
+							setBudgetsText(prev => ({
+								...prev,
+								[props.type]: e.target.value,
+							}));
+							setBudgets(prev => ({
+								...prev,
+								[props.type]: parseInt(e.target.value),
+							}));
+						}}
 					/>
 					<Progress value={(usage / budget) * 100} />
 				</div>
